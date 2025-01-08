3fdee6e185e6dedc1234120f7f918024bc0b4e05
Shengxiang Lin <2792674719@qq.com>
2025-01-08 22:40:31 +0800

    update

---
+++ 林圣翔Ex2/Assets/NewBehaviourScript3.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NewBehaviourScript3
+{
+    public static int sumplane= 1;
+    public static long tsum=0,esum=1,dsum=0;
+    public static float stime=0,ntime=0;
+}
