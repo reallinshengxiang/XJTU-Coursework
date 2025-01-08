55124da40ebc64000fb45fdc50094c8baced46e9
Shengxiang Lin <2792674719@qq.com>
2025-01-08 22:42:13 +0800

    update

---
+++ 林圣翔Ex2/Assets/NewBehaviourScript2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NewBehaviourScript2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
