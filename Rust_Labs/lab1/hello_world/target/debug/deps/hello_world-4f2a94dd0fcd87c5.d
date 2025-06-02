d0d6ea31b8a92c37b886748fe77845f39ed74d59
Shengxiang Lin <2792674719@qq.com>
2025-06-03 00:10:17 +0800

    upload

---
+++ Rust_Labs/lab1/hello_world/target/debug/deps/hello_world-4f2a94dd0fcd87c5.d
@@ -0,0 +1,5 @@
+/home/linshengxiang/rustproject/hello_world/target/debug/deps/hello_world-4f2a94dd0fcd87c5: src/main.rs
+
+/home/linshengxiang/rustproject/hello_world/target/debug/deps/hello_world-4f2a94dd0fcd87c5.d: src/main.rs
+
+src/main.rs:
---
+++ Rust_Labs/lab1/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/linshengxiang/rustproject/hello_world/target/debug/hello_world: /home/linshengxiang/rustproject/hello_world/src/main.rs
